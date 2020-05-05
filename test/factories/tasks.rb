fcb8f41faed32ee6ea87db698ffca7f5133ca7c9
ildarnarbek <narbekovildar13@gmail.com>
2020-05-05 16:53:58 +0400

    added states and rubocop -a

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :task do
-    name { "MyString" }
-    description { "MyText" }
+    name { 'MyString' }
+    description { 'MyText' }
     author_id { 1 }
     assignee_id { 1 }
-    state { "MyString" }
-    expired_at { "2020-05-04" }
+    state { 'MyString' }
+    expired_at { '2020-05-04' }
   end
 end
