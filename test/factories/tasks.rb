0b2806b0cdf27ce8dc37c987525b7f861806bcfb
ildarnarbek <narbekovildar13@gmail.com>
2020-05-09 16:49:23 +0400

    cleared, rubocop autofixed

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -2,14 +2,8 @@ FactoryBot.define do
   factory :task do
     name
     description
-    # author_id { 1 }
-    # assignee_id { 1 }
     state
     expired_at
-    # association :manager
-    # association :manager
-    # factory :manager do
-    # end
     association :author, factory: :manager
     association :assignee, factory: :manager
   end
