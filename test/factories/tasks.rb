3a6a7a8925fe4fc362f88c8cc4c6f0c4f83f0893
ildarnarbek <narbekovildar13@gmail.com>
2020-05-09 16:46:27 +0400

    added validations and associations

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -2,9 +2,15 @@ FactoryBot.define do
   factory :task do
     name
     description
-    author_id { 1 }
-    assignee_id { 1 }
+    # author_id { 1 }
+    # assignee_id { 1 }
     state
     expired_at
+    # association :manager
+    # association :manager
+    # factory :manager do
+    # end
+    association :author, factory: :manager
+    association :assignee, factory: :manager
   end
 end
