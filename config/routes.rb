4a6ed25b50f2e9bc37460bbe10ac71f7f3ff047b
ildarnarbek <narbekovildar13@gmail.com>
2020-05-17 00:30:17 +0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
     resources :users
   end
   
+  namespace :api do
+    namespace :v1 do
+      resources :tasks, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
+  
   root :to => "web/boards#show"
 
   scope module: :web do
