613406b7c9cbc1c7a7c126384a0daf75c74e0c51
ildarnarbek <narbekovildar13@gmail.com>
2020-05-16 21:27:27 +0400

    updated tests

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -1,50 +1,64 @@
 require 'test_helper'
 
 class Admin::UsersControllerTest < ActionController::TestCase
-  
+
   setup do
     admin = create(:admin)
     sign_in admin
   end
-  
-  test "should get show" do
+
+  test 'should get show' do
     user = create(:user)
     get :show, params: { id: user.id }
     assert_response :success
   end
-  
+
   test 'should get index' do
     get :index
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     user = create(:user)
     get :edit, params: { id: user.id }
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should post create" do
+  test 'should post create' do
     user = attributes_for(:user)
     post :create, params: { user: user }
     assert_response :redirect
+    
+    created_user = User.find_by(first_name: user[:first_name])
+    created_user.present?
+    assert created_user[:last_name] == user[:last_name]
+    assert created_user[:email] == user[:email]
+    assert created_user[:type] == user[:type]
   end
- 
-  test "should patch update" do
+
+  test 'should patch update' do
     user = create(:user)
     user_attrs = attributes_for(:user)
     patch :update, params: { id: user.id, user: user_attrs }
     assert_response :redirect
+
+    updated_user = User.find(user.id
+    assert updated_user[:first_name] == user_attrs[:first_name]
+    assert updated_user[:last_name] == user_attrs[:last_name]
+    assert updated_user[:email] == user_attrs[:email]
+    assert updated_user[:type] == user_attrs[:type]
   end
 
-  test "should delete destroy" do
+  test 'should delete destroy' do
     user = create(:user)
-    delete :destroy, params: {id: user.id }
+    delete :destroy, params: { id: user.id }
     assert_response :redirect
+
+    assert_not User.exists?(user.id)
   end
 end
