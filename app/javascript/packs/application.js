330437569ee7cd057cec142026840f381a92ed92
ildarnarbek <narbekovildar13@gmail.com>
2020-05-18 11:30:42 +0400

    added kanban board

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,4 +3,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'material-design-lite/material.js'
+import WebpackerReact from 'webpacker-react'
+import TaskBoard from './components/TaskBoard'
 
+WebpackerReact.setup({ TaskBoard })
