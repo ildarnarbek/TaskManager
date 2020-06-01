09e957ea573cf3f2ce28e7b6e27a6cfed2befc1e
ildarnarbek <narbekovildar13@gmail.com>
2020-06-01 17:36:28 +0400

    add users API

--- app/javascript/components/EditPopup/EditPopup.js
+++ app/javascript/components/EditPopup/EditPopup.js
@@ -49,6 +49,7 @@ const EditPopup = ({ cardId, onClose, onCardDestroy, onLoadCard, onCardUpdate })
     });
   };
   const isLoading = isNil(task);
+  const handleChangeSelect = fieldName => user => onChange({ ...task, [fieldName]: user });
 
   return (
     <Modal className={styles.modal} open onClose={onClose}>
--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :tasks, only: [:index, :show, :create, :update, :destroy]
+      resources :users, only: [:index, :show]
     end
   end
+
+  
   
   root :to => "web/boards#show"
 
