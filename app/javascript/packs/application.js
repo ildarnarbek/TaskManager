29d9aae47a182c4bd14c76ef7f7e1f65be24b4f6
ildarnarbek <narbekovildar13@gmail.com>
2020-06-07 04:32:26 +0400

    fix  cant find application error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ require('@rails/ujs').start();
 require('@rails/activestorage').start();
 require('channels');
 
+import 'stylesheets/application';
 import 'material-design-lite/material.js';
 import WebpackerReact from 'webpacker-react';
 import TaskBoard from 'components/TaskBoard';
