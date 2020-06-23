2d93f313aec910fe59df6c10dbac052a23abab76
ildarnarbek <narbekovildar13@gmail.com>
2020-06-23 17:57:00 +0400

    added limit for mailer. changed Usermailer on job only for task controller

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,8 +9,10 @@ require_relative '../config/environment'
 require 'rails/test_help'
 require 'sidekiq/testing'
 
+
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
   include ActionMailer::TestHelper
   include AuthHelper
+  Sidekiq::Testing.inline!
 end
