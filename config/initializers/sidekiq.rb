5b2a03dca0fa1042e3cba0d07e6a3648f5267db7
ildarnarbek <narbekovildar13@gmail.com>
2020-06-23 17:57:00 +0400

    added limit for mailer. changed Usermailer on job only for task controller

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,14 @@
 require 'sidekiq/web'
+require "sidekiq/throttled"
+require "sidekiq/throttled/web"
 
 Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL'] }
-  end
-  
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_URL'] }
-  end
+  config.redis = { url: ENV['REDIS_URL'] }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDIS_URL'] }
+end
+
+Sidekiq::Throttled.setup!
+Sidekiq::Throttled::Registry.add(:mailer, { threshold: { limit: 1, period: 5.seconds } })
