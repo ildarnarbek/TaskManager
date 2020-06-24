f4e7b4a551c7b3e9976b445f2e698ad0659b57f2
ildarnarbek <narbekovildar13@gmail.com>
2020-06-24 15:51:38 +0400

    added  limits

--- app/jobs/send_password_reset_notification_job.rb
+++ app/jobs/send_password_reset_notification_job.rb
@@ -1,12 +1,12 @@
 class SendPasswordResetNotificationJob < ApplicationJob
-    sidekiq_options queue: :mailers
-    sidekiq_throttle_as :mailer
-  
-    def perform(email)
-      @user = User.find_by(email: email)
-      return if @user.blank?
-  
-      UserMailer.with({ user: @user }).password_reset.deliver_now
-    end
+  sidekiq_options queue: :mailers
+  sidekiq_throttle_as :mailer
+  sidekiq_options lock: :until_and_while_executing, on_conflict: { client: :log, server: :reject }
+
+  def perform(email)
+    @user = User.find_by(email: email)
+    return if @user.blank?
+
+    UserMailer.with({ user: @user }).password_reset.deliver_now
   end
-  
+end
